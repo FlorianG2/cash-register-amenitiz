a60dcef24e7741db0aba77ec34b85988ba165eec
Florian Guerin <guerinflorian02@gmail.com>
2023-09-07 13:24:03 +0200

    creation of the product validations and tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    it 'is not valid without a product_code' do
+      product = Product.new(product_code: nil, name: 'Green Tea', price: 3.11)
+      expect(product).to_not be_valid
+    end
+
+    it 'is not valid without a name' do
+      product = Product.new(product_code: 'GR1', name: nil, price: 3.11)
+      expect(product).to_not be_valid
+    end
+
+    it 'is not valid without a price' do
+      product = Product.new(product_code: 'GR1', name: 'Green Tea', price: nil)
+      expect(product).to_not be_valid
+    end
+
+    it 'is valid with product_code, name and price' do
+      product = Product.new(product_code: 'GR1', name: 'Green Tea', price: 3.11)
+      expect(product).to be_valid
+    end
+  end
 end
